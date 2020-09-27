fcc4c22ca212c0375a17a4118ea4c94d7f68720a
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2020-09-27 22:02:15 +1000

    Update 9_PalindromeNumber.py

--- leetcode/9_PalindromeNumber.py
+++ leetcode/9_PalindromeNumber.py
@@ -1,3 +1,13 @@
+#PalindromNumber
+# example1
+# Input: 121
+# Output: true
+#
+# example2
+# Input: -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
