3e8036dbf099b20cd2588fd2ba9b74d5e581c166
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2020-11-03 22:31:54 +1100

    Update frequency_histogram_lab1.py

--- my_lab/frequency_histogram_lab1.py
+++ my_lab/frequency_histogram_lab1.py
@@ -1,6 +1,6 @@
 # frequency_histogram.py
 # 1. Asks the user for the input file's name;
-# 2. Read the file (if possible ensure that you enable the file error handling with strerror()) and counts all the Latin letters( lower- and upper-case letters are treated as equal)
+# 2. Read the file (enable the file error handling with strerror()) and counts all the Latin letters(lowerCase and upperCase letters are treated as equal)
 # 3. The histogram should be sent to a file the same name as the input one, but with the suffix '.hist'). 
 
 from os import strerror
