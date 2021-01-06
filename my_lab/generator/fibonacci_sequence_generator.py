133fe05ac3ae28a0d172e921bb0f4a289dad8896
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-01-06 22:04:20 +1100

    Create fibonacci_sequence_generator.py

---
+++ my_lab/generator/fibonacci_sequence_generator.py
@@ -0,0 +1,9 @@
+def fibonacci_gen():
+    trailing, lead = 0, 1
+    while True:
+        yield lead
+        trailing, lead = lead, trailing + lead
+
+fib = fibonacci_gen()
+for _ in range(10):
+    print(fib.__next__())
